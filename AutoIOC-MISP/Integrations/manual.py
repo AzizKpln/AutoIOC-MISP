5961429d02f0d6dcbbf921f7ebf26cb058b7d3ba
Aziz Kaplan <AzizKpln@protonmail.com>
2023-12-23 13:41:44 +0300

    Delete AutoIOC-MISP directory

--- AutoIOC-MISP/Integrations/manual.py
+++
@@ -1,7 +0,0 @@
-from Integrations.mispIntegration import *
-def runManually(mispapi,mispurl,mispeventid,ioclist):
-    misp_connect(mispapi,mispurl,mispeventid)
-    ioclist_=list(ioclist)
-    iocs=ioclist_[0].split("\r")
-    for i in iocs:
-        upload_attr(i.strip())
--- AutoIOC-MISP/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt update 
-apt upgrade -y
-apt install python3 -y
-apt install python3-pip -y
-python3 -m pip install requirements.txt
-clear
-echo "[+]Requirements For AutoMISP downloaded successfully!"
